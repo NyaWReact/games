6b93b8c39ca91f992a7bff1df12619ffee2b3731
Denis Davudov <den_davydov_d@vk.com>
2024-07-24 18:35:30 +0700

    change bgMenu and fonts

--- src/components/layout/menu/Menu.tsx
+++ src/components/layout/menu/Menu.tsx
@@ -12,7 +12,7 @@ const Menu: FC<IMenu> = () => {
   return (
     <div className={styles.menu}>
       <h1 className={styles.title}>Nya_Menu</h1>
-      <nav className={styles.navigate}>
+      <nav className={styles.navigate}> 
         <Link to={'/findPair'} className={styles.link}>Find_Pair</Link>      
         <Link to={'/gallows'} className={styles.link}>Gallows</Link>      
       </nav>
